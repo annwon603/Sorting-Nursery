9802d2b3b9f961bd8b14da93d2268383d519e354
annwon603 <aniwong603@gmail.com>
2024-06-28 13:13:46 -0700

    Improving the Click and Drag Script WIP

--- Assets/Scripts/DrapDrop.cs
+++ Assets/Scripts/DrapDrop.cs
@@ -8,24 +8,28 @@ public class DragDop : MonoBehaviour
     [SerializeField] private bool isDragging = false; 
 
 
-    public GameObject correctForm; // The box we want to put in
+    // public GameObject correctForm; // The box we want to put in
 
-    public bool finished ;
+    public bool finished ; //check if egg is in the box
     // private CanvasGroup canvasGroup;
     // public void OnPointerDown(PointerEventData eventData){
     //     Debug.Log("OnPointerDown");
     // }
 
-    private Vector3 resetPosition;
+    private Vector3 resetPosition; 
 
-    private Vector4 originalColor;
+    private Color originalColor;
+    private Color hover; //Intereaction for player to see if egg is hover over the box
 
 
     void Start()
     {
         resetPosition = this.transform.localPosition;
         Debug.Log("" + resetPosition.x);
-        originalColor = correctForm.GetComponent<SpriteRenderer>().color;
+        // originalColor = correctForm.GetComponent<SpriteRenderer>().color;
+        hover = originalColor;
+        hover.a = 0.9f;
+        
     }
 
     private void Awake(){
@@ -46,32 +50,32 @@ public class DragDop : MonoBehaviour
         isDragging = true;
         // Detecting if the item is within the box 
 
-        if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 1.5f &&
-            Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 1.5f)
-        {
-            Debug.Log("At the box");
-            correctForm.GetComponent<SpriteRenderer>().color = new Color(251,135,255,255);
-            finished = true;
+        // if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 1.5f &&
+        //     Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 1.5f)
+        // {
+        //     Debug.Log("At the box");
+        //     correctForm.GetComponent<SpriteRenderer>().color = hover;
+        //     finished = true;
             
-        }
-        else
-        {
-            Debug.Log("Not in the box");
-            correctForm.GetComponent<SpriteRenderer>().color = originalColor;
-            finished = false; 
-        }
+        // }
+        // else
+        // {
+        //     Debug.Log("Not in the box");
+        //     correctForm.GetComponent<SpriteRenderer>().color = originalColor;
+        //     finished = false; 
+        // }
     }
 
     public void OnMouseUp()
     {
         isDragging = false;
-        if(finished == true)
-        {
-            Debug.Log("In the box");
-            gameObject.SetActive(false);
-        } else {
-            this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
-        }
+        // if(finished == true)
+        // {
+        //     Debug.Log("In the box");
+        //     gameObject.SetActive(false);
+        // } else {
+        //     this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
+        // }
 
 
     }
--- Assets/Scripts/IncubatorSlot.cs
+++ Assets/Scripts/IncubatorSlot.cs
@@ -1,12 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+// using UnityEngine.EventSystems;
 
-public class IncubatorSlot : MonoBehaviour, IDropHandler
+public class IncubatorSlot : MonoBehaviour
 {
-   public void OnDrop(PointerEventData eventData){
+   private Collider2D z_Collider;
+   [SerializeField]
+   private ContactFilter2D z_Filter;
+   private List<Collider2D> z_CollidedObjects = new List<Collider2D>(1);
 
-    Debug.Log("OnDrop");
+   private Color originalColor;
+   private Color hover; //Intereaction for player to see if egg is hover over the box
+
+   private bool z_Intereacted = false;
+   private void Start()
+   {
+      z_Collider = GetComponent<Collider2D>();
+      originalColor = GetComponent<SpriteRenderer>().color;
+      hover = originalColor;
+      hover.a = 0.9f;
    }
+
+   private void Update()
+   {
+      z_Collider.OverlapCollider(z_Filter, z_CollidedObjects);
+      foreach(var o in z_CollidedObjects)
+      {
+         OnCollided(o.gameObject);
+      }
+   }
+
+   private void OnCollided(GameObject collidedObject)
+   {
+      // Debug.Log("Collided with " + collidedObject.name);
+      if(Input.GetMouseButton(0))
+      {
+         OnInteract();
+      }
+   }
+
+   private void OnInteract()
+   {
+      // if z_interacted is false
+      if(!z_Intereacted){
+         z_Intereacted = true;
+         Debug.Log("Left button being held down");
+      }else{
+         
+      }
+      
+   }
+
+
+
+   // public void OnDrop(PointerEventData eventData){
+
+   //  Debug.Log("OnDrop");
+   // }
 }
