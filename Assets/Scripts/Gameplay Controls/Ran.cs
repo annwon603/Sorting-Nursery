f4f59982b3021387f64c9172b76b83698a4cebf2
annwon603 <101071975+annwon603@users.noreply.github.com>
2024-07-16 16:37:47 -0700

    Fixed Bugs and Adjusted Chicken Probablity

--- Assets/Scripts/Gameplay Controls/Ran.cs
+++ Assets/Scripts/Gameplay Controls/Ran.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Ran : MonoBehaviour
 {
-    [SerializeField] private int ran;               // Store random integer
+    [SerializeField] private float ran;             // Store random integer
     [SerializeField] private bool doesTurn = false; // Decides if chicken should move egg
-    
+        
+    [SerializeField] private bool isBig;            // Set trait of chicken
+
+    [SerializeField] private bool doesMatch;        //Check if egg match chicken
     // Chicken interaction with the egg 
+
+    [SerializeField] private int adjustProp;       //  Adjusting Probablity for chicken to mess up
+
+    private void Start()
+    {
+        adjustProp = 20;        //Default probablity is 20%
+    }
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Egg"){
             
-            ran = Random.Range(0,2);                
-            Debug.Log("Random Value: " + ran);
+            ran = Random.value * 100;                
+            Debug.Log(gameObject.name + " Random Value: " + ran);
+            
+            bool ranBool = ProbabilityCheck(ran);
+            bool doesMatch = traitCheck(other.gameObject.GetComponent<DragDrop>().egg.Big);
+            Debug.Log(gameObject.name + " Is Random: " + ranBool);
+            Debug.Log(gameObject.name + " does match: " + doesMatch);
 
-            if(ran == 1){
+            if(doesMatch ^ ranBool == true){
                 doesTurn = true;
                 Debug.Log(gameObject.name + " move egg to right");
             }else{
@@ -26,5 +41,23 @@ public class Ran : MonoBehaviour
             other.GetComponent<Move>().doesTurn = doesTurn;
             
         }
+    }
+
+    public bool ProbabilityCheck(float ranVal)
+    {
+        if (ranVal <= adjustProp)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool traitCheck(bool size)
+    {
+        bool match = (size && isBig);
+        return match;
     }
 }
