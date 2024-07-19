ec0348f0cd0e5466dbfd4b066c0b087c440918a1
harry23yu <harry23yu@gmail.com>
2024-07-19 00:57:25 -0700

    7/18 Update
    Fixed the "show timer" option in the settings menu. The player can now choose if they want to turn the timer on or off.

--- Assets/Scripts/showTimer.cs
+++ Assets/Scripts/showTimer.cs
@@ -1,3 +1,78 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+
+// public class showTimer : MonoBehaviour
+// {
+//     public Toggle isTimerOn;
+//     public GameObject TimeInfo;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         // DontDestroyOnLoad(isTimerOn);
+//         if (!(isTimerOn.isOn)) {
+//             Debug.Log("Timer is off");
+//             TimeInfo.SetActive(false); 
+//         }
+//         if (isTimerOn.isOn) {
+//             Debug.Log("Time is on");
+//             TimeInfo.SetActive(true);
+//         }
+//     }
+// }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+
+// public class showTimer : MonoBehaviour
+// {
+//     public Toggle isTimerOn;
+//     public GameObject TimeInfo;
+//     public static bool timerState = false; // Default value
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         if (isTimerOn != null)
+//         {
+//             isTimerOn.isOn = timerState; // Set the toggle to the saved state
+//         }
+
+//         UpdateTimerVisibility();
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (isTimerOn != null)
+//         {
+//             timerState = isTimerOn.isOn; // Save the state of the toggle
+//             UpdateTimerVisibility();
+//         }
+//     }
+
+//     void UpdateTimerVisibility()
+//     {
+//         if (TimeInfo != null)
+//         {
+//             TimeInfo.SetActive(timerState);
+//             Debug.Log(timerState ? "Timer is on" : "Timer is off");
+//         }
+//     }
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,29 +82,31 @@ using UnityEngine.SceneManagement;
 public class showTimer : MonoBehaviour
 {
     public Toggle isTimerOn;
-    public GameObject TimerSetting;
     public GameObject TimeInfo;
+    public static bool timerVisible = false; // By default, the timer is not visible
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        isTimerOn.isOn = timerVisible;
+        UpdateTimerVisibility();
     }
 
     // Update is called once per frame
     void Update()
     {
-        DontDestroyOnLoad(TimerSetting);
-        if (!(isTimerOn.isOn)) {
-            Debug.Log("Timer is off");
-            TimeInfo.SetActive(false); 
-        }
-        if (isTimerOn.isOn) {
-            Debug.Log("Time is on");
+        timerVisible = isTimerOn.isOn;
+        UpdateTimerVisibility();
+    }
+
+    //Determines if the timer is visible or not and saves the data when switching scenes
+    void UpdateTimerVisibility()
+    {
+        if (timerVisible) {
             TimeInfo.SetActive(true);
+        }
+        else {
+            TimeInfo.SetActive(false);
         }
     }
 }
-
-//Ask Annie on Thursday if I can have the isTimerOn and timerSetting to be prefabs so that all the scenes
-//can use them
