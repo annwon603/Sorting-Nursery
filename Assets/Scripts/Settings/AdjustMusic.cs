787f5f88c9fa1488a3397599f7c57125e8ca185c
harry23yu <harry23yu@gmail.com>
2024-07-19 22:27:40 -0700

    7/19 Update
    Started to work on the music and SFX settings.

---
+++ Assets/Scripts/Settings/AdjustMusic.cs
@@ -0,0 +1,36 @@
+//Menu music got from Annie
+//Source used: https://www.youtube.com/watch?v=XE4ovK5o9tA&ab_channel=TheCryptoGym-AustinPatkos%28APex%29
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AdjustSounds : MonoBehaviour
+{
+
+    public Slider volumeSlider;
+    public AudioSource MenusMusic;
+    public static float menusMusicLevel = 7.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = menusMusicLevel;
+        UpdateMusicLevels();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        menusMusicLevel = volumeSlider.value;
+        UpdateMusicLevels();
+    }
+
+    void UpdateMusicLevels()
+    {
+        MenusMusic.volume = menusMusicLevel / 10;
+        Debug.Log("Volume is " + MenusMusic.volume);
+    }
+}
---
+++ Assets/Scripts/Settings/EnableSFX.cs
@@ -0,0 +1,41 @@
+//Button click SFX got from https://pixabay.com/sound-effects/search/button-click/
+//Source used: https://www.youtube.com/watch?v=HtiWt0SWxk8&ab_channel=xxRafaelProductions-RafaelVicuna
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnableSFX : MonoBehaviour
+{
+
+    public Toggle isSFXOn;
+    public static bool SFXOn = true; //By default, the SFX should be on
+    public AudioSource ButtonClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSFXOn.isOn = SFXOn;
+        UpdateSFXSounds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SFXOn = isSFXOn.isOn;
+        UpdateSFXSounds();
+    }
+
+    public void UpdateSFXSounds()
+    {
+        if (SFXOn) {
+            ButtonClick.Play();
+            Debug.Log("SFX is on");
+        }
+        else {
+            Debug.Log("SFX is off");
+        }
+    }
+}
