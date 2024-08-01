c64c2f513836111984961cae876aa95d09187f55
annwon603 <101071975+annwon603@users.noreply.github.com>
2024-08-01 16:56:29 -0700

    Improved Tutorial Level
    - Lever can switch direction of the egg
    - Hide the timer for the tutorial
    - Added an eggshell background at the beginning so that it can transition to the work scene
    - Added in new dialogue UI asset
    - Need to fix bug for the basket UI

--- Assets/Scripts/IncubatorSlot.cs
+++ Assets/Scripts/IncubatorSlot.cs
@@ -71,6 +71,27 @@ public class IncubatorSlot : MonoBehaviour
       }
    }
 
+   //For just comparing the Egg through UI
+   public void Compare()
+   {
+      List<UIBasketItem> eggsToCompare = FindObjectOfType<BasketPage>().listOfBasketItems;
+      foreach(var egg in eggsToCompare)
+      {
+         bool doesMatch = traitMatch(egg.trait.isTraitActive, egg.trait.traitSet);
+         if(doesMatch == true){
+               Debug.Log("In the right box");
+               ScoreCounter.instance.IncreaseScore();
+            }else{
+               Debug.Log("In the wrong box");
+         }
+         egg.ResetData();
+         //egg.gameObject.SetActive(false);
+      }
+
+      FindObjectOfType<BasketPage>().ClearItem();
+      
+   }
+
    public bool traitMatch(bool trait, string traitSet)
    {
       //First it checks if the boolean value "isTraitActive" of both incubator
