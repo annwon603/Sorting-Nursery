55cf3fc6f70cae998b711ab4d27ca697554dd4fc
annwon603 <aniwong603@gmail.com>
2024-08-21 19:05:27 -0700

    80% Done with Implementing Sounds
    Need to implement creature SFX at the playground
    Fix UI

--- Assets/Scripts/Level/LvlManager.cs
+++ Assets/Scripts/Level/LvlManager.cs
@@ -15,6 +15,12 @@ public class LvlManager : MonoBehaviour
     public resultsScript Result;
     public GameObject beginLevel;
 
+    
+    public FMODUnity.EventReference CompleteMan;
+    public FMODUnity.EventReference CompleteObj;
+
+
+
     [SerializeField]
     Objective objective;
 
@@ -164,10 +170,12 @@ public class LvlManager : MonoBehaviour
         if(currManEggs >= requiredManEggs)
         {
             quotaManager.CompleteMandatory();
+
             if(justIncreaseDinoScore == false)
             {
                 PlayGround.IncreaseDragonScore();
                 justIncreaseDinoScore = true;
+                FMODUnity.RuntimeManager.PlayOneShot(CompleteMan);
             }
             
         }
@@ -175,10 +183,12 @@ public class LvlManager : MonoBehaviour
         if(currOptEggs >= reqeuiredoptEggs)
         {
             quotaManager.CompleteOpt();
+
             if(justIncreaseDragonScore == false)
             {
                 PlayGround.IncreaseDinoScore();
                 justIncreaseDragonScore = true;
+                FMODUnity.RuntimeManager.PlayOneShot(CompleteObj);
             }
         }
 
