73ef36dfab69499cb6d42e3a3adb7e8948310746
annwon603 <aniwong603@gmail.com>
2024-08-21 22:03:30 -0700

    Implemented All Sounds (Except 2 music track)

--- Assets/Scripts/UI/VcaController.cs
+++ Assets/Scripts/UI/VcaController.cs
@@ -6,11 +6,30 @@ public class VcaController : MonoBehaviour
 {
     // Start is called before the first frame update
     private FMOD.Studio.VCA vcaController;
+
+    [SerializeField]
     private string path;    // Ex: "vca:/Master:" without the Paretheseis
+
+    public Toggle toggle;
 
     void Start()
     {
         vcaController = FMODUnity.RuntimeManager.GetVCA("vca:/" + path);
+        toggle.onValueChanged.AddListener(OnToggleValueChanged);
+    }
+
+    public void OnToggleValueChanged(bool isOn)
+    {
+        if (isOn)
+        {
+            TurnOn();
+            Debug.Log("Turn on");
+        }
+        else
+        {
+            TurnOff();
+            Debug.Log("Turn off");
+        }
     }
 
     // Update is called once per frame
